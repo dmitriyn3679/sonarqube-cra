4a69ef7e308ee18ba17b4af325c5232fe5d03b7d
DmytroNikolenko <dmitriyn3679@gmail.com>
2025-01-14 17:07:30 +0200

    duplicate func

--- src/components/DuplicateComponent2.jsx
+++ src/components/DuplicateComponent2.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 const DuplicateComponent2 = () => {
+  const getNumber = () => {
+    const number = 123;
+    return number;
+  };
+  
+  const getNumber2 = () => {
+    const number = 123;
+    return number;
+  };
+  
+  getNumber();
+  getNumber2();
+  
   return (
     <div>
       <p>This is a duplicated component.</p>
